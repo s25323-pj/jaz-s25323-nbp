631a32d99e9f70a866a2b99a45adf1734ff759f6
s25323-pj <s25323@pjwstk.edu.pl>
2023-06-24 12:42:55 +0200

    Initial commit

---
+++ jaz-s25323-nbp/src/main/java/com/SzyRosz/jazs25323nbp/JazS25323NbpApplication.java
@@ -0,0 +1,13 @@
+package com.SzyRosz.jazs25323nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS25323NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS25323NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s25323-nbp/src/test/java/com/SzyRosz/jazs25323nbp/JazS25323NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SzyRosz.jazs25323nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS25323NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
